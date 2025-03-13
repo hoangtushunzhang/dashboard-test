77f1ed25ce64d9e15342d81267f471fd74f0f569
Nguyen Xuan Quang <quang456a@gmail.com>
2025-03-13 10:42:16 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "MyPress",
+  description: "Admin dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar />
+        <div className="flex">
+          <div className="hidden md:block">
+            <Sidebar />
+          </div>
+          <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+const Navbar = () => {
+  return (
+    <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
+      <Link href="/">
+        <Image src={"/logo.svg"} width={40} height={40} alt="logo" />
+      </Link>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none cursor-pointer">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback className="text-black">Shun</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>Log Out</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default Navbar;
